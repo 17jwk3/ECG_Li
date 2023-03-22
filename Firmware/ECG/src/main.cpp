d509d95e9c9ae55dfe7e6d1c21a491713556a4d0
JackAttack <60326772+17jwk3@users.noreply.github.com>
2023-03-22 00:04:25 -0400

    Update main.cpp

--- Firmware/ECG/src/main.cpp
+++ Firmware/ECG/src/main.cpp
@@ -1,19 +1,28 @@
 #include <Arduino.h>
-// initialize the serial communication:
-void setup(){
-  Serial.begin(230400);
+#include "BluetoothSerial.h"
+
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ECG"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
   pinMode(22, INPUT); // Setup for leads off detection LO +
   pinMode(23, INPUT); // Setup for leads off detection LO -
+  pinMode(26, INPUT); // Setup for leads off detection LO -
 }
 
 void loop() {
-if((digitalRead(22) == 1)||(digitalRead(23) == 1)){
-  Serial.println('!');
-}
-else{
-// send the value of analog input 0:
-  Serial.println(analogRead(26));
-}
-//Wait for a bit to keep serial data from saturating
+  int data;
+  data  = analogRead(26);
+  // send the value of analog input 0:
+  SerialBT.println(data);
+  Serial.println(data);
+  //Wait for a bit to keep serial data from saturating
   delay(1);
 }
